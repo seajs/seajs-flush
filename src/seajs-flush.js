62a67f70e55eba535bba189d61dbab77aa41e7e8
lifesinger <lifesinger@gmail.com>
2013-06-24 21:29:22 +0800

    Refactor flush code

--- src/seajs-flush.js
+++ src/seajs-flush.js
@@ -3,73 +3,54 @@
  */
 (function(seajs) {
 
-  var slice = [].slice
-  var _use = seajs.use
+  var Module = seajs.Module
+  var _load = Module.prototype._load
+
+  var data = seajs.data
+  var cid = 0
   var stack = []
 
-  seajs.use = function(ids, callback) {
-    stack.push([isArray(ids) ? ids : [ids], callback])
-    return seajs
+
+  Module.prototype._load = function() {
+    stack.push(this)
   }
 
   seajs.flush = function() {
-    var ids = []
-    var callbacks = []
+    var deps = []
 
-    for (var i = 0; i < stack.length; i++) {
-      var args = stack[i]
-      ids[i] = args[0]
-      callbacks[i] = args[1]
+    // Collect dependencies
+    for (var i = 0, len = stack.length; i < len; i++) {
+      deps = deps.concat(stack[i].dependencies)
     }
 
-    stack.length = 0
+    // Create an anonymous module for flushing
+    var mod = Module.get(
+        uri || data.cwd + "_flush_" + cid++,
+        deps
+    )
 
-    return _use(flatten(ids), function() {
-      var args = group(slice.call(arguments), ids)
+    mod._load = _load
 
-      for (var i = 0; i < callbacks.length; i++) {
-        var callback = callbacks[i]
-        if (callback) {
-          callback.apply(null, args[i])
-        }
+    mod._callback = function() {
+      for (var i = 0, len = stack.length; i < len; i++) {
+        stack[i]._onload()
       }
-    })
-  }
 
-  function flatten(arr, ret) {
-    ret || (ret = [])
-
-    for (var i = 0; i < arr.length; i++) {
-      var item = arr[i]
-      isArray(item) ? flatten(item, ret) : ret.push(item)
+      // Empty stack
+      stack.length = 0
     }
 
-    return ret
+    // Load it
+    mod._load()
   }
 
-  function group(args, ids) {
-    var ret = []
-    var item, arr
-
-    for (var i = 0; i < ids.length; i++) {
-      ret[i] = arr = []
-      item = ids[i]
-
-      for (var j = 0; j < item.length; j++) {
-        arr[j] = args.shift()
-      }
-    }
-
-    return ret
-  }
-
-  var toString = {}.toString
-
-  var isArray = Array.isArray || function(obj) {
-    return toString.call(obj) === "[object Array]"
-  }
+  // Flush on loaded
+  seajs.on("requested", function() {
+    seajs.flush()
+  })
 
 
+  // Register as module
   define("seajs-flush", [], {})
 
 })(seajs);
